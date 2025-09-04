a2e081dcf92c403aa2bd4e0d0b089029d863ca56
Muzzu153 <muzzusyed153@gmail.com>
2025-09-04 17:27:24 +0530

    Add backendurl and path to build dir

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
+  baseURL: import.meta.env.NODE_ENV === "development" ? "http://localhost:4000/api" : "https://gup-shup-i0o3.onrender.com",
   withCredentials: true,
 });
